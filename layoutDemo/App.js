499853fdc961f45dc3706d607959fab944e62ce4
Magesh <tkmagesh77@gmail.com>
2019-05-28 12:32:30 +0530

    updated App.js

--- layoutDemo/App.js
+++ layoutDemo/App.js
@@ -8,12 +8,18 @@
 
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
-import BoxModelDemo from './src/BoxModelDemo1';
+//import FlexBoxLayout from './src/FlexboxJustifyContentCenter';
+//import FlexBoxLayout from './src/FlexboxJustifyContentFlexEnd';
+//import FlexBoxLayout from './src/FlexboxJustifyContentFlexStart';
+//import FlexBoxLayout from './src/FlexboxJustifyContentSpaceAround';
+//import FlexBoxLayout from './src/FlexboxJustifyContentSpaceBetween';
+//import FlexBoxLayout from './src/FlexboxAlignSelf1';
+import FlexBoxLayout from './src/FlexboxAlignSelf2';
 
 export default class App extends Component<Props> {
   render() {
     return (
-      <BoxModelDemo/>
+      <FlexBoxLayout/>
     );
   }
 }
